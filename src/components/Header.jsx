e5ce6af7e8bbfeeca4ad34dfa01815ff707232fa
Jose Diaz <schmuckejgd@example.com>
2024-05-22 17:16:30 -0400

    reto-completado

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import logo from '../assets/images/logo.svg'
+import  Navbar  from '../components/NavBar'
+
+const Header = () => {
+  return (
+    <header className='flex place-content-between items-center mb-14'>
+      <img className='cursor-pointer' src={logo} alt="logo" />
+      <Navbar />
+    </header>
+  )
+}
+
+export default Header
